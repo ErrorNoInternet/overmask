266144b23800dc74c44f974601b3aa625dadd405
ErrorNoInternet <errornointernet@envs.net>
2023-09-24 15:00:52 +0800

    Add full flake

--- flake.nix
+++ flake.nix
@@ -4,39 +4,51 @@
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
         mozilla.url = "github:mozilla/nixpkgs-mozilla";
+        flake-utils.url = "github:numtide/flake-utils";
     };
 
-    outputs = { self, nixpkgs, mozilla }:
-    let
-        system = "x86_64-linux";
-        overlays = [ self.inputs.mozilla.overlays.rust ];
-        pkgs = import nixpkgs { inherit overlays system; };
-        pkgsStatic = pkgs.pkgsStatic;
-        pkgsCross = pkgs.pkgsCross;
-        channel = pkgs.rustChannelOf {
-            date = "2023-09-16";
-            channel = "nightly";
-            sha256 = "sha256-FCEJYhy/e7g2X2f90Ek32bFJkyIKguIIvT/hqpoFuNI=";
-        };
-        rust = (channel.rust.override {
-            targets = [
-                "x86_64-unknown-linux-musl"
-            ];
-            extensions = [ "rust-src" ];
-        });
-    in
-    {
-        devShells.${system}.default = pkgs.mkShell {
-            name = "rust-environment";
-            buildInputs = [
-                rust
-                pkgs.pkg-config
-                pkgs.libusb1
-            ];
+    outputs = { self, nixpkgs, mozilla, flake-utils }:
+        (flake-utils.lib.eachDefaultSystem (system:
+            let
+                overlays = [ self.inputs.mozilla.overlays.rust ];
+                pkgs = import nixpkgs { inherit system overlays; };
+                channel = pkgs.rustChannelOf {
+                    date = "2023-09-16";
+                    channel = "nightly";
+                    sha256 = "sha256-FCEJYhy/e7g2X2f90Ek32bFJkyIKguIIvT/hqpoFuNI=";
+                };
+                rust = (channel.rust.override {
+                    targets = [
+                        "x86_64-unknown-linux-musl"
+                    ];
+                    extensions = [ "rust-src" ];
+                });
+            in rec
+            {
+                devShells.${system}.default = pkgs.mkShell {
+                    name = "rust-environment";
+                    nativeBuildInputs = [
+                        pkgs.pkg-config
+                    ];
+                    buildInputs = [
+                        rust
+                        pkgs.udev
+                    ];
 
-            PKG_CONFIG_ALLOW_CROSS = true;
-            PKG_CONFIG_ALL_STATIC = true;
-            LIBZ_SYS_STATIC = 1;
-        };
-    };
+                    PKG_CONFIG_ALLOW_CROSS = true;
+                    PKG_CONFIG_ALL_STATIC = true;
+                    LIBZ_SYS_STATIC = 1;
+                };
+
+                packages.overmask = pkgs.rustPlatform.buildRustPackage {
+                    pname = "overmask";
+                    version = "0.1.5";
+                    cargoLock.lockFile = ./Cargo.lock;
+                    src = pkgs.lib.cleanSource ./.;
+                    nativeBuildInputs = [ pkgs.pkg-config ];
+                    buildInputs = [ pkgs.udev ];
+                };
+                defaultPackage = packages.overmask;
+            }
+        ));
 }
