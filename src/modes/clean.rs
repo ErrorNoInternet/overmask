975becc7c54345929310e1c946fddc7dc9650a74
ErrorNoInternet <errornointernet@envs.net>
2024-02-09 20:32:38 +0800

    feat(clean): truncation

--- src/modes/clean.rs
+++ src/modes/clean.rs
@@ -2,11 +2,13 @@ use crate::Files;
 use std::{os::unix::fs::FileExt, process::exit};
 
 pub fn main(files: &Files, truncate: bool) {
+    println!("deduplicating seed and overlay files...");
+
     let mut seed_buffer = vec![0; files.block_size as usize];
     let mut overlay_buffer = vec![0; files.block_size as usize];
     let zeros = vec![0; files.block_size as usize];
-
     let mut blocks_freed = 0;
+
     let mut last_percent = 0.0;
     let block_limit = files.seed_size / u64::from(files.block_size);
     for block in 0..block_limit {
@@ -63,9 +65,60 @@ pub fn main(files: &Files, truncate: bool) {
             blocks_freed += 1;
         }
     }
-
     println!(
         "successfully zeroed {blocks_freed} blocks ({} bytes)",
         blocks_freed * files.block_size
     );
+
+    if truncate {
+        do_truncate(files);
+    };
+}
+
+fn do_truncate(files: &Files) {
+    println!("locating end of mask file...");
+
+    let mut mask_buffer = vec![0; files.block_size as usize];
+    let mut end_of_file = None;
+
+    let mut last_percent = 0.0;
+    let block_limit = files.mask_size / u64::from(files.block_size);
+    for block in (0..block_limit).rev() {
+        #[allow(clippy::cast_precision_loss)]
+        let percent = 100.0 - block as f64 / block_limit as f64 * 100.0;
+        if percent - last_percent > 0.1 {
+            last_percent = percent;
+            println!("checking blocks: {:.1}% ({block}/{block_limit})", percent);
+        }
+
+        let offset = block * u64::from(files.block_size);
+        if let Err(error) = files.mask.read_at(&mut mask_buffer, offset) {
+            eprintln!(
+                "overmask: couldn't read {} bytes from mask file at offset {offset}: {error}",
+                files.block_size
+            );
+            if files.ignore_errors {
+                continue;
+            }
+            exit(1);
+        }
+        if !mask_buffer.iter().all(|&byte| byte == 0) {
+            break;
+        }
+        end_of_file = Some(offset);
+    }
+    if let Some(offset) = end_of_file {
+        println!("truncating mask and overlay files to {offset} bytes...");
+        if let Err(error) = files.mask.set_len(offset) {
+            eprintln!("overmask: couldn't truncate mask file to {offset} bytes: {error}");
+            exit(1);
+        };
+        if let Err(error) = files.overlay.set_len(offset) {
+            eprintln!("overmask: couldn't truncate overlay file to {offset} bytes: {error}");
+            exit(1);
+        };
+        println!("successfully truncated mask and overlay files to {offset} bytes");
+    } else {
+        println!("no unused block found");
+    }
 }
